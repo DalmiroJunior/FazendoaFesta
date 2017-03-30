9948647520dec99966d3f22c2e38df2855c71749
Henrique Couto <rick.couto@hotmail.com>
2017-03-30 08:45:02 -0300

    Toolbar fix

--- app/src/main/java/dalmirojunior/fazendoafesta/MainActivity.java
+++ app/src/main/java/dalmirojunior/fazendoafesta/MainActivity.java
@@ -3,6 +3,8 @@ package dalmirojunior.fazendoafesta;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,14 +12,18 @@ import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
     private String [] eventos = {"Casamento","Aniversario infantil", "Aniversario de 15 anos"};
     private ListView listViewEventos;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.customToolbar);
+        setSupportActionBar(toolbar);
+
         listViewEventos = (ListView) findViewById(R.id.ListEventos);
 
         // ArrayAdapter<String> adapter = new ArrayAdapter(getApplicationContext(),
@@ -39,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
