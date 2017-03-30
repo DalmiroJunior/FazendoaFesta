ac3c73fcda1f2f0263d018f66ca1929ebea10f2c
Henrique Couto <rick.couto@hotmail.com>
2017-03-30 09:07:57 -0300

    Toolbar fix

--- app/src/main/java/dalmirojunior/fazendoafesta/AniversarioActivity.java
+++ app/src/main/java/dalmirojunior/fazendoafesta/AniversarioActivity.java
@@ -2,6 +2,7 @@ package dalmirojunior.fazendoafesta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class AniversarioActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class AniversarioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aniversario);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.customToolbar);
+        setSupportActionBar(toolbar);
     }
 }
--- app/src/main/java/dalmirojunior/fazendoafesta/AniversarioInfActivity.java
+++ app/src/main/java/dalmirojunior/fazendoafesta/AniversarioInfActivity.java
@@ -2,6 +2,7 @@ package dalmirojunior.fazendoafesta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class AniversarioInfActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class AniversarioInfActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aniversario_inf);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.customToolbar);
+        setSupportActionBar(toolbar);
     }
 }
