298d4598e8021f5b236b76020929320ae733bded
Henrique C. Tatagiba <rick.couto@hotmail.com>
2017-05-30 13:49:04 -0300

    layout

--- app/src/main/java/nof/fazendoafesta/EmpresaInfNatalieDecoracoesDC1Activity.java
+++ app/src/main/java/nof/fazendoafesta/EmpresaInfNatalieDecoracoesDC1Activity.java
@@ -26,7 +26,7 @@ public class EmpresaInfNatalieDecoracoesDC1Activity extends AppCompatActivity {
         toolbar.setTitle("Contato");
         setSupportActionBar(toolbar);
 
-        callButton = (ImageButton) findViewById(R.id.imageButtonLigarInf);
+        callButton = (ImageButton) findViewById(R.id.imageButtonLigar);
         callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
